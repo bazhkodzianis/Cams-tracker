8ae99645c0c8b1b30f3a241e3566620e7118fc17
bazhkodzianis <bazhkodzianis@gmail.com>
2026-09-03 17:58:53 -0400

    Target fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = "cam-sets-20260903-2114";
+var CACHE = "cam-sets-20260903-2153";
 var SHELL = [
   "./", "./index.html", "./manifest.webmanifest",
   "./icon-192.png", "./icon-512.png", "./icon-maskable-512.png"
