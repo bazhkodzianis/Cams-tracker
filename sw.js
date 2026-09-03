be50d7e4cf7caafc0ec12debba252d0b14a47f7e
bazhkodzianis <bazhkodzianis@gmail.com>
2026-09-03 12:06:54 -0400

    Dark theme

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = "cam-sets-20260903-0325";
+var CACHE = "cam-sets-20260903-0357";
 var SHELL = [
   "./", "./index.html", "./manifest.webmanifest",
   "./icon-192.png", "./icon-512.png", "./icon-maskable-512.png"
