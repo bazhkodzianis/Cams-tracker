9bfa3d83f0e3eedefbf304cb041122092bce3390
bazhkodzianis <bazhkodzianis@gmail.com>
2026-09-02 23:09:28 -0400

    Settings added and hidded

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = "cam-sets-20260903-0259";
+var CACHE = "cam-sets-20260903-0307";
 var SHELL = [
   "./", "./index.html", "./manifest.webmanifest",
   "./icon-192.png", "./icon-512.png", "./icon-maskable-512.png"
